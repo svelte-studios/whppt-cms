55db6772e9a6763887e0920de0deca92fde995f6
Terence Kruger <terence@sveltestudios.com>
2018-10-24 08:16:01 +1030

    Add support for AWS elastic search

--- src/ElasticSearch/index.js
+++ src/ElasticSearch/index.js
@@ -3,10 +3,12 @@ import User from "./User";
 import Image from "./Image";
 
 const elasticsearch = require("elasticsearch");
+const elasticsearchaws = require("aws-elasticsearch-client");
 
 export default (config, options) => {
   const elastic_config = JSON.parse(JSON.stringify(config));
-  const $elastic = new elasticsearch.Client(elastic_config);
+  let $elastic = new elasticsearch.Client(elastic_config);
+  if (config.aws) $elastic = new elasticsearchaws(elastic_config);
 
   return {
     User: User({ $elastic, $options: options }),
